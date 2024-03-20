bcbf35e3ed22a5df9154691bac0d041c0a58e657
Gilberson Rickelmy Amaral Campos <87537403+RickelmyCampos@users.noreply.github.com>
2024-03-20 14:39:20 -0300

    feat: basic challenger

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,34 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome:String,val idade:Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    private val inscritos = mutableListOf<Usuario>()
+
+    fun matricular(vararg usuario: Usuario) {
+        inscritos.addAll(usuario)
+
     }
+
+    fun listarInscritos(): List<Usuario> = inscritos
+
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val listContent = listOf(
+        ConteudoEducacional("introducao kotlin", duracao = 120, nivel = Nivel.BASICO),
+        ConteudoEducacional("android kotlin", duracao = 120, nivel = Nivel.INTERMEDIARIO)
+    )
+    val kotlin = Formacao("Kotlin", conteudos = listContent,Nivel.INTERMEDIARIO)
+
+    val gil=Usuario("Gil",20)
+    val rick=Usuario("Rick", idade = 26)
+    kotlin.matricular(gil,rick)
+    print(" lista de inscritos: ${kotlin.listarInscritos()}")
+ 
 }
+
